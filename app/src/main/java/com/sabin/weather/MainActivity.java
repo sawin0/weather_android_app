6db095f6c105fa2330a452e9395fc7b808d86725
Sabin <sawin0>
2024-01-16 08:10:03 +0545

    binding recyclerview and data passing completed

--- app/src/main/java/com/sabin/weather/MainActivity.java
+++ app/src/main/java/com/sabin/weather/MainActivity.java
@@ -1,6 +1,8 @@
 package com.sabin.weather;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 import android.widget.ImageView;
@@ -9,8 +11,10 @@ import android.widget.TextView;
 import com.bumptech.glide.Glide;
 
 import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.Date;
+import java.util.List;
 import java.util.Locale;
 
 public class MainActivity extends AppCompatActivity {
@@ -32,6 +36,32 @@ public class MainActivity extends AppCompatActivity {
 
         ImageView tempIV = findViewById(R.id.tempIV);
         Glide.with(this).load("https://goo.gl/gEgYUd").into(tempIV);
+
+
+        // creating dummy weather data list
+        List<Weather> weatherList = new ArrayList<>();
+        weatherList.add(new Weather("", "today", "clear", "70", "35"));
+        weatherList.add(new Weather("", "tomorrow", "clear", "70", "35"));
+        weatherList.add(new Weather("", "3rd day", "clear", "70", "35"));
+        weatherList.add(new Weather("", "today", "clear", "70", "35"));
+        weatherList.add(new Weather("", "today", "clear", "70", "35"));
+
+
+
+
+
+        // Lookup the recyclerview in activity layout
+        RecyclerView weatherRV = (RecyclerView) findViewById(R.id.weatherRV);
+
+        // Initialize contacts
+//        contacts = Weather.createContactsList(20);
+        // Create adapter passing in the sample user data
+        WeatherAdapter adapter = new WeatherAdapter(weatherList);
+        // Attach the adapter to the recyclerview to populate items
+        weatherRV.setAdapter(adapter);
+        // Set layout manager to position the items
+        weatherRV.setLayoutManager(new LinearLayoutManager(this));
+        // That's all!
 
     }
 }
