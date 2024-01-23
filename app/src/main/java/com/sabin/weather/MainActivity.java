fadfeeb3f04e49eb40db12583544cca42a95b1d3
Sabin <sawin0>
2024-01-23 08:01:53 +0545

    feat: displaying data on reecycler view completed

--- app/src/main/java/com/sabin/weather/MainActivity.java
+++ app/src/main/java/com/sabin/weather/MainActivity.java
@@ -30,6 +30,9 @@ import java.util.Locale;
 
 public class MainActivity extends AppCompatActivity {
 
+    List<Weather> weatherListData = new ArrayList<>();
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -52,25 +55,11 @@ public class MainActivity extends AppCompatActivity {
 
 
         // creating dummy weather data list
-        List<Weather> weatherList = new ArrayList<>();
-        weatherList.add(new Weather("", "today", "clear", "70", "35"));
-        weatherList.add(new Weather("", "tomorrow", "clear", "70", "35"));
-        weatherList.add(new Weather("", "3rd day", "clear", "70", "35"));
-        weatherList.add(new Weather("", "today", "clear", "70", "35"));
-        weatherList.add(new Weather("", "today", "clear", "70", "35"));
-
-        // Lookup the recyclerview in activity layout
-        RecyclerView weatherRV = (RecyclerView) findViewById(R.id.weatherRV);
-
-        // Initialize contacts
-//        contacts = Weather.createContactsList(20);
-        // Create adapter passing in the sample user data
-        WeatherAdapter adapter = new WeatherAdapter(weatherList, this);
-        // Attach the adapter to the recyclerview to populate items
-        weatherRV.setAdapter(adapter);
-        // Set layout manager to position the items
-        weatherRV.setLayoutManager(new LinearLayoutManager(this));
-        // That's all!
+//        weatherList.add(new Weather("", "today", "clear", "70", "35"));
+//        weatherList.add(new Weather("", "tomorrow", "clear", "70", "35"));
+//        weatherList.add(new Weather("", "3rd day", "clear", "70", "35"));
+//        weatherList.add(new Weather("", "today", "clear", "70", "35"));
+//        weatherList.add(new Weather("", "today", "clear", "70", "35"));
     }
 
 
@@ -86,27 +75,34 @@ public class MainActivity extends AppCompatActivity {
                     public void onResponse(String response) {
                         // Display the first 500 characters of the response string.
                         //textView.setText("Response is: " + response.substring(0,500));
-                        Log.d("fetchWeather",response.toString());
                         JSONObject responseObject = null;
                         try {
                             responseObject = new JSONObject(response);
                             JSONArray weatherList = responseObject.getJSONArray("list");
                             Log.d("TAG", "onResponse: weatherList: " + weatherList.toString());
+                            Log.d("TAG", "onResponse: hello");
                             // Iterate through the weather list
-//                            for (int i = 0; i < weatherList.length(); i++) {
-//                                JSONObject weatherObject = weatherList.getJSONObject(i);
-//
-//                                // Extract relevant information for each entry
-//                                String dateTime = weatherObject.getString("dt_txt");
-//                                JSONObject mainObject = weatherObject.getJSONObject("main");
-//                                double temperature = mainObject.getDouble("temp");
-//
-//                                // Handle the extracted information as needed
-//                                // You can update your UI or perform other actions here
-//                            }
+                            for (int i = 0; i < weatherList.length(); i++) {
+                                JSONObject weatherObject = weatherList.getJSONObject(i);
+
+                                // Extract relevant information for each entry
+                                String dateTime = weatherObject.getString("dt");
+                                String temMin = weatherObject.getJSONObject("main").getString("temp_min");
+                                String temMax = weatherObject.getJSONObject("main").getString("temp_max");
+                                String status = weatherObject.getJSONArray("weather").getJSONObject(0).getString("main");
+                                String imageUrl = weatherObject.getJSONArray("weather").getJSONObject(0).getString("icon");
+
+                                // adding data to weatherList
+                                weatherListData.add(new Weather(imageUrl, dateTime, status, temMax, temMin));
+
+
+                                // Handle the extracted information as needed
+                                // You can update your UI or perform other actions here
+                            }
                         } catch (JSONException e) {
                             throw new RuntimeException(e);
                         }
+                        setUpRV();
 
 
                     }
@@ -119,5 +115,20 @@ public class MainActivity extends AppCompatActivity {
 
 // Add the request to the RequestQueue.
         queue.add(stringRequest);
+    }
+
+    void setUpRV(){
+        // Lookup the recyclerview in activity layout
+        RecyclerView weatherRV = (RecyclerView) findViewById(R.id.weatherRV);
+
+        // Initialize contacts
+//        contacts = Weather.createContactsList(20);
+        // Create adapter passing in the sample user data
+        WeatherAdapter adapter = new WeatherAdapter(weatherListData, this);
+        // Attach the adapter to the recyclerview to populate items
+        weatherRV.setAdapter(adapter);
+        // Set layout manager to position the items
+        weatherRV.setLayoutManager(new LinearLayoutManager(this));
+        // That's all!
     }
 }
