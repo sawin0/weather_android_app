05da9303fc4169d0e8ab19db669655120b02b540
Sabin <sawin0>
2024-01-29 21:01:08 +0545

    fix: parsing datetime from unixtime

--- app/src/main/java/com/sabin/weather/Weather.java
+++ app/src/main/java/com/sabin/weather/Weather.java
@@ -1,6 +1,11 @@
 package com.sabin.weather;
 
+import java.time.Instant;
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
+import java.util.Date;
 
 public class Weather {
     String imageUrl;
@@ -12,7 +17,12 @@ public class Weather {
 
     public Weather(String imageUrl, String day, String status, String maxTemp, String minTemp) {
         this.imageUrl = "https://openweathermap.org/img/wn/"+ imageUrl + "@2x.png";
-        this.day = day;
+        LocalDateTime date = LocalDateTime.ofInstant(Instant.ofEpochSecond(Long.parseLong(day)), ZoneId.systemDefault());
+        // Define a custom date-time formatter
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd, MMM HH:mm");
+
+        // Format the LocalDateTime using the formatter
+        this.day = date.format(formatter);
         this.status = status;
         this.maxTemp = maxTemp;
         this.minTemp = minTemp;
